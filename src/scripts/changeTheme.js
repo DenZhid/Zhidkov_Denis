a96758783926039fd7016e9324311355967f0d6b
Денис Жидков <denis.zhidkov01@gmail.com>
2021-10-26 23:13:15 +0300

    BEM and themes

---
+++ src/scripts/changeTheme.js
@@ -0,0 +1,7 @@
+function changeTheme() {
+    if (document.body.classList.contains("dark-theme")) {
+        document.body.classList.replace("dark-theme", "light-theme");
+    } else {
+        document.body.classList.replace("light-theme", "dark-theme");
+    }
+}
