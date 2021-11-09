ed6084ff2a39dfe51d19759ffab55a78de44022e
Денис Жидков <denis.zhidkov01@gmail.com>
2021-11-09 16:25:22 +0300

    Всё исправил, всё работает

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -1,8 +1,5 @@
 document.addEventListener("DOMContentLoader", changeTheme);
 function changeTheme() {
-    if (document.body.classList.contains("dark-theme")) {
-        document.body.classList.replace("dark-theme", "light-theme");
-    } else {
-        document.body.classList.replace("light-theme", "dark-theme");
-    }
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
 }
