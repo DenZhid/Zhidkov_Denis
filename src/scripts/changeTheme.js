764ed96eb97d9478ba45ce0af8b459537ba2bfa9
Денис Жидков <denis.zhidkov01@gmail.com>
2021-11-09 15:53:07 +0300

    Almost all fixes

--- src/scripts/changeTheme.js
+++ src/scripts/changeTheme.js
@@ -1,7 +1,4 @@
 function changeTheme() {
-    if (document.body.classList.contains("dark-theme")) {
-        document.body.classList.replace("dark-theme", "light-theme");
-    } else {
-        document.body.classList.replace("light-theme", "dark-theme");
-    }
+    document.body.classList.toggle("light-theme");
+    document.body.classList.toggle("dark-theme");
 }
