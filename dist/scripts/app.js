2223caf51d8ce1faeeb1df2519595d363ffce89b
Денис Жидков <denis.zhidkov01@gmail.com>
2021-11-29 15:28:26 +0300

    Homework №4

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,5 +1,6 @@
-document.addEventListener("DOMContentLoader", changeTheme);
-function changeTheme() {
-    document.body.classList.toggle("light-theme");
-    document.body.classList.toggle("dark-theme");
-}
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.addEventListener("click", function () {
+        document.body.classList.toggle("light-theme");
+        document.body.classList.toggle("dark-theme");
+    });
+});
